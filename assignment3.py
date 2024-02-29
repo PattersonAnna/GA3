488fc738f73cf3088ff11e0dc0795d2ff2ae3170
Anna Patterson <156390769+PattersonAnna@users.noreply.github.com>
2024-02-28 16:51:14 -0800

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -8,15 +8,22 @@ def minimun_cost_connecting_edges(input_file_path, output_file_path):
         rows = [l.replace('(', '').replace(')', '').split(',') for l in infile.readlines()]
         vertices = [(int(x), int(y)) for x, y in zip(*[iter(rows[0])] * 2)]
         edges = [(int(x), int(y)) for x, y in zip(*[iter(rows[1])] * 2)]
-        print(vertices[1])
 
+
+
+
+
+
+        manhatan_distance(vertices[0], vertices[3])
         #vertices = [Point(int(x), int(y)) for x, y in zip(*[iter(rows[0])] * 2)] 
         #edges = [Point(int(x), int(y)) for x, y in zip(*[iter(rows[1])] * 2)] 
 
 
 def manhatan_distance(vertice_start, vertice_end):
-
-    #total_weigth = 
+    x_value_start, y_value_start = vertice_start
+    x_value_end, y_value_end = vertice_end
+    total_weigth = abs(x_value_start - x_value_end) + abs(y_value_start - y_value_end)
+    return total_weigth
         
 
 
@@ -26,3 +33,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
