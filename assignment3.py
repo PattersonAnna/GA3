8edf5c2c7c22b616a075d28cafe9c00cdcd260af
Anna Patterson <156390769+PattersonAnna@users.noreply.github.com>
2024-02-29 22:38:26 -0800

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -11,13 +11,16 @@ def minimun_cost_connecting_edges(input_file_path, output_file_path):
     with open(output_file_path, 'w') as outfile:
         outfile.write(f'{int(total_weight)}\n')
     
-
+#takes in two (x, y) values and calculates the manhatan distance
 def manhatan_distance(vertice_start, vertice_end):
     x_value_start, y_value_start = vertice_start
     x_value_end, y_value_end = vertice_end
     total_weigth = abs(x_value_start - x_value_end) + abs(y_value_start - y_value_end)
     return total_weigth
 
+#created an adjaceny matrix that has row and column = number of vertices, first for loop sets the indexs to the corisponding manhatan distance
+#second for loop chnages the given edges to to .1 because the nature of the graph an edge will never be less then 1 and zero represent the distance 
+#from a vertice to itself, algorthm over looks matrix values that equal 0
 def create_adjacency_matrix(vertices, edges):
     num_vertices = len(vertices)
     adjacency_matrix = [[0.0] * num_vertices for _ in range(num_vertices)]
@@ -28,9 +31,9 @@ def create_adjacency_matrix(vertices, edges):
         edge_start, edge_end = edge
         adjacency_matrix[int(edge_start-1)][int(edge_end-1)] = 0.1
         adjacency_matrix[int(edge_end-1)][int(edge_start-1)] = 0.1
-
     return adjacency_matrix
 
+#finds the index that has not yet been set to have the smallest weight
 def min_edge_weight(graph, weight_of_path, visited):
     num_vertices = len(graph)
     current_min = 10000000000000
@@ -40,6 +43,8 @@ def min_edge_weight(graph, weight_of_path, visited):
             current_vertices = i
     return current_vertices
 
+#Creates a list that holds the values of the edges with the least amout of weight that creates a complete graph
+# uses for loops and a helper function to move through the adjacency matrix findng the path with the least amount of weight
 def minimun_graph_weight(graph):
     num_vertices = len(graph)
     weight_of_path = [1000000000000] * num_vertices                              #used to store the the different weigth options
@@ -52,20 +57,16 @@ def minimun_graph_weight(graph):
         for j in range(num_vertices):
             if graph[i][j] > 0 and visited[j] == False and weight_of_path[j] > int(graph[i][j]):
                 weight_of_path[j] = graph[i][j]
-
     return weight_of_path
 
+# A for loop that only counts the values that aren't 0.1 because 0.1 = the given edges weight and they should actually be zero
 def correct_weight(minimun_cost):
     total = len(minimun_cost)
     sum = 0
     for i in range(total):
-        if minimun_cost[i] == 0.1:
-            minimun_cost[i] = 0
-    for i in range(total):
-        sum += minimun_cost[i]
-
+        if minimun_cost[i] != 0.1:
+            sum += minimun_cost[i]
     return sum
-
     
 def main():
     minimun_cost_connecting_edges("Sample_Input.txt", "Sample_Output.txt")
