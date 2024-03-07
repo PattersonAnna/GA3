274bc3d1c2a3bfa435ae88f92fa069402c7b783d
Anna Patterson <156390769+PattersonAnna@users.noreply.github.com>
2024-03-06 16:38:58 -0800

    Create assignment4.py

---
+++ assignment4.py
@@ -0,0 +1,9 @@
+def can_turn_off_lights(input_file_path, output_file_path):
+    pass
+
+
+def main():
+    can_turn_off_lights("Sample_Input.txt", "Sample_Output.txt")
+
+if __name__ == "__main__":
+    main()
